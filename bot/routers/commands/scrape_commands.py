c37552f69b532347a35c4bb90044d80e5c9e2532
Jeyw1n <135513247+Jeyw1n@users.noreply.github.com>
2025-08-07 14:48:25 +0300

    Add logging

--- bot/routers/commands/scrape_commands.py
+++ bot/routers/commands/scrape_commands.py
@@ -2,6 +2,8 @@ from aiogram import Router
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 
+from loguru import logger
+
 from scrapers import scrape_posts
 
 router = Router(name=__name__)
@@ -11,7 +13,11 @@ async def start_command(message: Message) -> None:
     await message.answer(text="Парсинг начался!")
     posts = scrape_posts()
     for i in posts:
+        print("перебор сгенерированных постов")
         if len(i) > 1000:
             print(i)
             continue
-        await message.answer(text=i, parse_mode="HTML")
+        try:
+            await message.answer(text=i)
+        except Exception as ex:
+            logger.error(f"Ошибка отправки обработанного текста: {ex}")
